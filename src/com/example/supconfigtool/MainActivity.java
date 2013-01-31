6485176541f1767d9e658f216da13ec6d3e42d9f
javroo <markus.crap@gmail.com>
2013-02-01 00:20:54 +0100

    Removed some EditText code no longer used.

--- src/com/example/supconfigtool/MainActivity.java
+++ src/com/example/supconfigtool/MainActivity.java
@@ -223,20 +223,6 @@ public class MainActivity extends Activity {
         }
     }
 
-    // The action listener for the EditText widget, to listen for the return key
-    private TextView.OnEditorActionListener mWriteListener =
-        new TextView.OnEditorActionListener() {
-        public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
-            // If the action is a key-up event on the return key, send the message
-            if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
-                String message = view.getText().toString();
-                sendMessage(message);
-            }
-            if(D) Log.i(TAG, "END onEditorAction");
-            return true;
-        }
-    };
-
     private final void setStatus(int resId) {
         final ActionBar actionBar = getActionBar();
         actionBar.setSubtitle(resId);
